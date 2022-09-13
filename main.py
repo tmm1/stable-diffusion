3930f20ca49ce6829172ed542e0a48ff202aca89
Aman Karmani <aman@tmm1.net>
2022-09-12 19:31:50 -0700

    do not used distributed gloo mode

--- main.py
+++ main.py
@@ -696,7 +696,7 @@ if __name__ == '__main__':
         # merge trainer cli with config
         trainer_config = lightning_config.get('trainer', OmegaConf.create())
         # default to ddp
-        trainer_config['strategy'] = 'ddp'
+        trainer_config['strategy'] = 'dp'
         for k in nondefault_trainer_args(opt):
             trainer_config[k] = getattr(opt, k)
         if not 'gpus' in trainer_config:
