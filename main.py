9a1107098d4fdedc44fa252801774b4f3e405d3c
Aman Karmani <aman@tmm1.net>
2022-09-12 20:12:11 -0700

    fix CUDACallback failure on epoch end

--- main.py
+++ main.py
@@ -554,7 +554,7 @@ class CUDACallback(Callback):
         #torch.cuda.synchronize(trainer.root_gpu)
         self.start_time = time.time()
 
-    def on_train_epoch_end(self, trainer, pl_module, outputs):
+    def on_train_epoch_end(self, trainer, pl_module, outputs=None):
         torch.cuda.synchronize(trainer.root_gpu)
         max_memory = (
             0 #torch.cuda.max_memory_allocated(trainer.root_gpu) / 2**20
